c36a20ca27f34023381b494b59341f36b3ef42ec
henie_yo@hotmail.com <henie_yo@hotmail.com>
2018-07-11 05:30:52 -0500

    se agrega splash y se mandan llamar elementos a index.js para comensar funcionalidad

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-console.log("hola");
+//console.log("hola");
+var btnCipher= document.getElementById("cipher");
+var btnDecipher= document.getElementById("decipher");
+var cipherText=document.getElementById("text-to-convert");
+
 
 
 $(document).ready(function(){
